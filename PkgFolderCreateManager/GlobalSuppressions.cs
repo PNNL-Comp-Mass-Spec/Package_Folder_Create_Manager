3f98eea063590a25c09b64c29540eb99e8fe544e
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-13 08:52:48 -0700

    Suppress warnings

--- PkgFolderCreateManager/GlobalSuppressions.cs
+++ PkgFolderCreateManager/GlobalSuppressions.cs
@@ -5,7 +5,11 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Keep for possible future use", Scope = "member", Target = "~F:PkgFolderCreateManager.clsFolderTools.WARN_IF_DIRECTORY_EXISTS")]
+[assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Justification = "Keep for possible future use", Scope = "member", Target = "~M:PkgFolderCreateManager.clsFolderCreateTask.SetFolderCreateTaskComplete(System.String,System.String,System.Int32,System.String,System.Int32)~System.Boolean")]
+[assembly: SuppressMessage("Redundancy", "RCS1213:Remove unused member declaration.", Justification = "Keep for possible future use", Scope = "member", Target = "~F:PkgFolderCreateManager.clsFolderTools.WARN_IF_DIRECTORY_EXISTS")]
 [assembly: SuppressMessage("Simplification", "RCS1103:Convert 'if' to assignment.", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:PkgFolderCreateManager.clsFolderTools.CreateDirectory(System.String,System.Collections.Generic.IReadOnlyDictionary{System.String,System.String},System.String)")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Keep for possible future use", Scope = "member", Target = "~M:PkgFolderCreateManager.clsFolderCreateTask.SetFolderCreateTaskComplete(System.String,System.String,System.Int32,System.String,System.Int32)~System.Boolean")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:PkgFolderCreateManager.clsBroadcastCmd")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:PkgFolderCreateManager.clsDbTask")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:PkgFolderCreateManager.clsFolderCreateTask")]
